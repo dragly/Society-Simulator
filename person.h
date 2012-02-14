6078571cad697cd26154c54f1e07c1892d4f6924
Vegard Johannesen <vegardjohannesen@gmail.com>
2012-02-14 23:41:14 +0100

    Rewriting the person class

--- person.h
+++ person.h
@@ -4,40 +4,64 @@
 #include "building.h"
 #include "math.h"
 
+//action
+enum PersonEvent {
+    None,
+    Move,
+    Work,
+    Rest,
+    Shop
+    //etc
+};
+
+class PersonStats {
+public:
+    PersonStats();
+    void finishEvent();
+    void createEvent();
+
+    double health() { return _health; }
+    double satiety() { return _satiety; }
+    double cash() { return _cash; }
+
+    PersonEvent personEvent() { return _personEvent; }
+    double duration() { return _duration; }
+    double evEnd() { return _evEnd; }
+    double evStart() { return _evStart; }
+    Building* currentBuilding() { return _currentBuilding; }
+    Building* destination() { return _destination; }
+
+
+private:
+    double _health; //rest
+    double _satiety; //hunger/food
+    double _cash;
+
+    PersonEvent _personEvent;
+    double _duration;
+    double _evEnd;
+    double _evStart;
+
+    Building* _currentBuilding;
+    Building* _destination;
+};
+
 class Person
 {
 public:
     Person();
 private:
-    //stats
-    double health; //rest
-    double satiety; //hunger/food
-    double cash;
 
-    //pos
-    //int x;
-    //int y;
+    PersonStats pS;
 
-    Building* currentBuilding;
     Building* workPlace;
     Building* localStore; //For now, only use one store
     Building* home;
 
-    //action
-    enum PersonEvent {
-        Move,
-        Work,
-        Rest,
-        Shop
-        //etc
-    };
-    double duration;
-    double end; //time of completion
-    PersonEvent personEvent;
     void processEvent();
 
-    getMoveTime(Building* from, Building* to);
-    getMoveTime(Building* target);
+    double  getMoveTime(Building* from, Building* to);
+    double getMoveTime(Building* target);
 
 };
 
